9191d0569b20a35ed424f8eb81aae769f8b5bab4
emanuelsantossouza <emanuelsantoss42@gmail.com>
2023-11-23 16:29:34 -0300

    Projeto tela inicial ManeulFlix

---
+++ js/script.js
@@ -0,0 +1,38 @@
+function mensagem() {
+    Swal.fire("Seja bem vindo!!");
+}
+
+let usuario = [
+    { nome: 'Gallo', foto: 'img/avatar1.png' },
+    { nome: 'Emanuel', foto: 'img/avatar4.png' },
+    { nome: 'Vítoria', foto: 'img/avatar5.jpg' },
+];
+
+let ul = document.querySelector('ul');
+
+for (let i = 0; i < usuario.length; i++) {
+    ul.innerHTML +=
+
+        `
+        <li>
+            <a href="#">
+                <div class="profile">
+                    <img src="${usuario[i].foto}" alt="${usuario[i].nome}">
+                <span>${usuario[i].nome}</span>
+                </div>
+            </a>
+        </li>  
+    `
+}
+
+ul.innerHTML += 
+            `
+                <li>
+                    <a href="#">
+                        <div class="profile">
+                            <i class='bx bxs-plus-circle'></i>
+                            <span>Adicionar perfil</span>
+                        </div>
+                    </a>
+                </li>
+            `
